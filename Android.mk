s4704/android_hardware_qcom_audio-caf-msm8996

omni: add build guard
Change-Id: Ie9e3493144ae6a37aa69797ca3861c878f4c240f

Android.mk
@@ -1,3 +1,4 @@
+ifeq ($(TARGET_QCOM_AUDIO_VARIANT),caf-msm8996)
 ifneq ($(filter mpq8092 msm8960 msm8226 msm8x26 msm8610 msm8974 msm8x74 apq8084 msm8916 msm8994 msm8992 msm8909 msm8996 msm8952 msm8937 thorium msm8953 msmgold msm8998 sdm660 sdm845 sdm710 apq8098_latv qcs605 msmnile $(MSMSTEPPE) $(TRINKET),$(TARGET_BOARD_PLATFORM)),)
 
 MY_LOCAL_PATH := $(call my-dir)
@@ -22,4 +23,5 @@ ifeq ($(USE_LEGACY_AUDIO_DAEMON), true)
 include $(MY_LOCAL_PATH)/audiod/Android.mk
 endif
 
+endif
 endif

choose-a/android_hardware_qcom_audio_msm8996-caf

Android.mk
@@ -1,3 +1,4 @@
+ifeq ($(TARGET_QCOM_AUDIO_VARIANT),msm8996-caf)
 ifneq ($(filter mpq8092 msm8960 msm8226 msm8x26 msm8610 msm8974 msm8x74 apq8084 msm8916 msm8994 msm8992 msm8909 msm8996 msm8952 msm8937 thorium msm8953 msmgold msm8998 sdm660 sdm845 sdm710 apq8098_latv qcs605 msmnile $(MSMSTEPPE) $(TRINKET) kona,$(TARGET_BOARD_PLATFORM)),)
 
 MY_LOCAL_PATH := $(call my-dir)
@@ -22,4 +23,5 @@ ifeq ($(USE_LEGACY_AUDIO_DAEMON), true)
 include $(MY_LOCAL_PATH)/audiod/Android.mk
 endif
 
+endif
 endif